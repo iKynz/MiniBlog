f05826ad505282bb05efd8fb3510c3c4b0442082
iKynz <emanuell_messias369@hotmail.com>
2022-12-15 15:24:52 -0300

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,69 @@
+import { NavLink } from "react-router-dom";
+import styles from "./Navbar.module.css";
+
+// Hooks
+import { useAuthentication } from "../hooks/useAuthentication";
+
+// Context
+import { useAuthValue } from "../context/AuthContext";
+
+const Navbar = () => {
+
+    const { user } = useAuthValue();
+    const {logout} = useAuthentication();
+
+    return (
+        <nav className={styles.navbar}>
+            <NavLink to="/" className={styles.brand}>
+                Mini <span>Blog</span>
+            </NavLink>
+            <ul className={styles.links_list}>
+                <li>
+                    <NavLink to="/" className={({ isActive }) => (isActive ? styles.active : "")}>
+                        Inicio
+                    </NavLink>
+                </li>
+                {!user && (
+                    <>
+                        <li>
+                            <NavLink to="/login" className={({ isActive }) => (isActive ? styles.active : "")}>
+                                Login
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/register" className={({ isActive }) => (isActive ? styles.active : "")}>
+                                Cadastre-se
+                            </NavLink>
+                        </li>
+                    </>
+                )}
+                {user && (
+                    <>
+                        <li>
+                            <NavLink to="/posts/create" className={({ isActive }) => (isActive ? styles.active : "")}>
+                                Novo Post
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/dashboard" className={({ isActive }) => (isActive ? styles.active : "")}>
+                                Dashboard
+                            </NavLink>
+                        </li>
+                    </>
+                )}
+                <li>
+                    <NavLink to="/about" className={({ isActive }) => (isActive ? styles.active : "")}>
+                        Sobre
+                    </NavLink>
+                </li>
+                {user && (
+                    <li>
+                        <button onClick={logout}>Sair</button>
+                    </li>
+                )}
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ src/pages/CreatePost/CreatePost.js
@@ -0,0 +1,119 @@
+import styles from "./CreatePost.module.css";
+
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthValue } from "../../context/AuthContext";
+import { useInsertDocument } from "../../hooks/useInsertDocument";
+
+const CreatePost = () => {
+
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [body, setBody] = useState("");
+  const [tags, setTags] = useState("");
+  const [formError, setFormError] = useState("");
+
+  const { user } = useAuthValue();
+
+  const { insertDocument, response } = useInsertDocument("posts");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormError("");
+
+    // validate image URL
+
+    try {
+      new URL(image);
+    } catch (error) {
+      setFormError("A imagem precisa ser uma URL");
+    }
+
+    // Criar o array de tags
+
+    const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase())
+
+    // Checar todos os valores
+
+    if (!title || !image || !tags || !body) {
+      setFormError("Por favor, preencha todos os campos");
+    }
+
+    if (formError) return;
+
+    insertDocument({
+      title,
+      image,
+      body,
+      tagsArray,
+      uid: user.uid,
+      createdBy: user.displayName
+    })
+
+    // redirect to home page
+    navigate("/");
+
+  }
+
+  return (
+    <div className={styles.create_post}>
+      <h2>Criar Post</h2>
+      <p>Escreva sobre o que quiser e compartilhe aquilo que te faz bem!</p>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Titulo:</span>
+          <input
+            type="text"
+            required
+            placeholder="Título da publicação"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            name="title" />
+        </label>
+
+        <label>
+          <span>URL da imagem:</span>
+          <input
+            type="text"
+            required
+            placeholder="Insira uma imagem"
+            onChange={(e) => setImage(e.target.value)}
+            value={image}
+            name="image" />
+        </label>
+
+        <label>
+          <span>Conteudo:</span>
+          <textarea
+            name="body"
+            required
+            placeholder="Insira o conteudo do post"
+            onChange={(e) => setBody(e.target.value)}
+            value={body}>
+          </textarea>
+        </label>
+
+        <label>
+          <span>Tags:</span>
+          <input
+            type="text"
+            required
+            placeholder="Insira tags separadas por vírgulas"
+            onChange={(e) => setTags(e.target.value)}
+            value={tags}
+            name="tags" />
+        </label>
+
+        {!response.loading && <button className="btn">Cadastrar</button>}
+        {response.loading && <button className="btn">Aguarde...</button>}
+        {response.error && <p className="error">{response.error}</p>}
+        {formError && <p className="error">{formError}</p>}
+      </form>
+    </div>
+  )
+}
+
+export default CreatePost
