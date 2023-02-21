03fea4a0853291ad8b596f03e5407468c88b72b7
Emanoel Messias <89670998+iKynz@users.noreply.github.com>
2023-02-21 19:03:57 -0300

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,7 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    
+    apiKey: "AIzaSyDv58zXZwI8d0EkMs8rkTkqh-jzdCnPD0U",
+    authDomain: "miniblog-fa688.firebaseapp.com",
+    projectId: "miniblog-fa688",
+    storageBucket: "miniblog-fa688.appspot.com",
+    messagingSenderId: "53137630548",
+    appId: "1:53137630548:web:23c3c87a4bfbdfa62a54c1"
 };
 
 
